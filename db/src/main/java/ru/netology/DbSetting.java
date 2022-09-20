ad3c53a9fd74de8abc1df710e1e2f222d0b3ea0b
Olga Fedorova <bamnu13@gmail.com>
2022-09-20 17:29:13 +0300

    Maven Task Initial Commit

---
+++ db/src/main/java/ru/netology/DbSetting.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+public class DbSetting {
+
+    private String name;
+    private String password;
+
+    public DbSetting(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+}
