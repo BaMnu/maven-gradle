5bbc795a56790fe8e1e0835133b7138586ee0d8d
Olga Fedorova <bamnu13@gmail.com>
2022-09-20 20:49:17 +0300

    Second Task Gradle

---
+++ db/src/main/java/ru/netology/DbSetting.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+public class DbSetting {
+
+    private String name;
+    private String password;
+
+    public DbSetting(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
